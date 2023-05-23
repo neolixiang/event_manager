ff7dfa6fe6e48d8bc22373b92407f34a3f71ba55
neolixiang <22746477+neolixiang@users.noreply.github.com>
2023-05-23 01:01:46 -0400

    nothing

--- lib/event_manager.rb
+++ lib/event_manager.rb
@@ -1,7 +1,14 @@
 puts 'Event Manager Initialized!'
 
 lines = File.readlines('event_attendees.csv')
+row_index = 0
 lines.each do |line|
-  puts line
+  row_index = row_index + 1
+  next if row_index == 1 
+    # the `next if` line checks every line to see if it matches the provided string. 
+    # If so, it skips that line from the rest of the loop.
+  columns = line.split(",")
+  names = columns[2]
+  puts names
 end
 
