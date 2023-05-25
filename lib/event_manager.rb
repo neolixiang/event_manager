ce16b78a685106a35fedb13e3732aa66808447e0
neolixiang <22746477+neolixiang@users.noreply.github.com>
2023-05-24 22:39:43 -0400

    found a way to use ipynb (jupyter notebook) to run ruby file in vs code.

--- lib/event_manager.rb
+++ lib/event_manager.rb
@@ -1,14 +1,7 @@
+require 'csv'
 puts 'Event Manager Initialized!'
 
-lines = File.readlines('event_attendees.csv')
-row_index = 0
-lines.each do |line|
-  row_index = row_index + 1
-  next if row_index == 1 
-    # the `next if` line checks every line to see if it matches the provided string. 
-    # If so, it skips that line from the rest of the loop.
-  columns = line.split(",")
-  names = columns[2]
-  puts names
-end
+contents = 
+
+
 
